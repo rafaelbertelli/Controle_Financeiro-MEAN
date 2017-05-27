f2a746bc90aaf39e14fe226f0741973015751af5
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-05-27 13:57:53 -0300

    App

---
+++ angular1/gulpTasks/deps.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
+
+gulp.task('deps.js', function() {
+    
+})
+
+gulp.task('deps.css', function() {
+    
+})
+
+gulp.task('deps.fonts', function() {
+    
+})  
---
+++ angular1/gulpTasks/server.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp')
+
+gulp.task('watch', ['watch'], function() {
+
+})
+
+gulp.task('watch', function() {
+
+})
