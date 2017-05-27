4d8a5d49b4d9d8b1479ff6b673dfb9bfac7d8b85
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-05-27 13:04:28 -0300

    Update

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const BillingCyle = require('./billingCycle')
 
 BillingCyle.methods(['get', 'post', 'put', 'delete'])
@@ -5,6 +6,25 @@ BillingCyle.updateOptions({
     new: true, 
     runValidators: true
 })
+
+BillingCyle.after('post', sendErrorOrNext).after('put', sendErrorOrNext)
+
+function sendErrorOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if(bundle.errors) {
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeRestFulErrors) {
+    const errors = []
+    _.forIn(nodeRestFulErrors, error => errors.push(error.message))
+    return errors
+}
 
 BillingCyle.route('count', function(req, res, next) {
     BillingCyle.count(function(error, value) {
