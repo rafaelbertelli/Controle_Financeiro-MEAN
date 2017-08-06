a969f82d38a6d3126c019d60ae82d857d41a367d
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-08-06 11:50:07 -0300

    'add'

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -13,7 +13,8 @@
 		vm.refresh = function() {
 			$http.get(url).then(function(response) {
 				vm.billingCycle = {}
-				vm.billingCycles = response
+				vm.billingCycles = response.data
+				console.log('metodo refresh: ', response.data)
 				tabs.show(vm, { tabList: true, tabCreate: true })
 			})
 		}
@@ -29,6 +30,20 @@
 
 			})
 
+		}
+
+		vm.showTabUpdate = function(billingCycle) {
+
+			vm.billingCycle = billingCycle
+			tabs.show(vm, { tabUpdate: true })
+			
+		}
+
+		vm.showTabDelete = function(billingCycle) {
+
+			vm.billingCycle = billingCycle
+			tabs.show(vm, { tabDelete: true })
+			
 		}
 
 		vm.refresh()
